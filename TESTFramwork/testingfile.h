29f5510099f2f5e89993c0d0cdacc446dbea6c68
salmangt <salman.ahmed@gaditek.com>
2018-04-13 13:25:10 +0500

    first commit

---
+++ TESTFramwork/testingfile.h
@@ -0,0 +1,13 @@
+//
+//  testingfile.h
+//  TESTFramwork
+//
+//  Created by Salman  Ahmed on 13/04/2018.
+//  Copyright © 2018 Atom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testingfile : NSObject
+
+@end
---
+++ TESTFramwork/testingfile.m
@@ -0,0 +1,13 @@
+//
+//  testingfile.m
+//  TESTFramwork
+//
+//  Created by Salman  Ahmed on 13/04/2018.
+//  Copyright © 2018 Atom. All rights reserved.
+//
+
+#import "testingfile.h"
+
+@implementation testingfile
+
+@end
