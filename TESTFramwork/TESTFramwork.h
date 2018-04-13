9be45fa1333a8ebedfa784ae6cd61f69d641ef06
salmangt <salman.ahmed@gaditek.com>
2018-04-13 13:00:49 +0500

    Initial Commit

---
+++ TESTFramwork/TESTFramwork.h
@@ -0,0 +1,19 @@
+//
+//  TESTFramwork.h
+//  TESTFramwork
+//
+//  Created by Salman  Ahmed on 13/04/2018.
+//  Copyright © 2018 Atom. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TESTFramwork.
+FOUNDATION_EXPORT double TESTFramworkVersionNumber;
+
+//! Project version string for TESTFramwork.
+FOUNDATION_EXPORT const unsigned char TESTFramworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TESTFramwork/PublicHeader.h>
+
+
